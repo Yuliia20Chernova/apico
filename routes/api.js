e93f1b60640eb91a0b4e7618395fe377f5d63b19
Yuliia20Chernova <yuliya20chernova@gmail.com>
2024-02-10 09:04:43 +0100

    some change

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const { app } = require('apico/server.js');
 /** Declare routes for Router */
+// api/
 router.get('/', async( req, res )=>{
+
+    res.json({"message": "APi works"});
+});
+
+// api/getname
+router.post('/getname', async( req, res )=>{
 
     res.json({"message": "APi works"});
 });
